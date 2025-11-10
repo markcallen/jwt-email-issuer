b3d9610369f99c69547ec46aae72080b3293be7a
Mark C Allen <mark@markcallen.com>
2025-11-10 13:44:50 -0800

    Fixed code to match the docs

--- examples/server/server.ts
+++ examples/server/server.ts
@@ -6,7 +6,7 @@ import { verifyToken } from 'jwt-email-issuer';
 const app = express();
 app.use(express.json());
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ['http://localhost:5173', 'http://localhost:4173'], credentials: true }));
 
 const jwtOptions = {
   issuer: 'com.example.issuer',
